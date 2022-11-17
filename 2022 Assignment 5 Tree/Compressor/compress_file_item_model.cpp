324a357389322713043582406c22a81375fc9a5e
AzurIce <973562770@qq.com>
2022-11-17 19:35:47 +0800

    clean up

--- 2022 Assignment 5 Tree/Compressor/compress_file_item_model.cpp
+++
@@ -1,9 +0,0 @@
-#include "compress_file_item_model.h"
-
-#include <QFileSystemModel>
-
-CompressFileItemModel::CompressFileItemModel(QObject *parent)
-    : QAbstractItemModel{parent}
-{
-    QFileSystemModel();
-}
